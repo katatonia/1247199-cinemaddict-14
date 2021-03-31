fa0fe8113e6b7196aa56043785d5908a5aeb6e97
Polina Milekhina <xxiiixviii@gmail.com>
2021-03-31 23:42:15 +0300

    Подключение bundle.js. Создание модулей и вставка компонентов в DOM

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createUserTemplate } from './view/user';
+import { createMenuTemplate } from './view/menu';
+import { createSortTemplate } from './view/sort';
+import { createFilmsList } from './view/films';
+import { createCardRate } from './view/films';
+
+const render = (container, place, template) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const header = document.querySelector('.header');
+render(header, 'beforeend', createUserTemplate());
+
+const main = document.querySelector('.main');
+render(main, 'beforeend', createMenuTemplate());
+render(main, 'beforeend', createSortTemplate());
+render(main, 'beforeend', createFilmsList());
+render(main, 'beforeend', createCardRate());
