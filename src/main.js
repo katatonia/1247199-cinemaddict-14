7a03157ee3062a5c48fba9df47146e68bc3e45d6
Polina Milekhina <xxiiixviii@gmail.com>
2021-04-06 16:09:47 +0300

    Добавляет функцию отрисовки экстра-блоков

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createUserTemplate } from './view/user';
 import { createMenuTemplate } from './view/menu';
 import { createSortTemplate } from './view/sort';
-import { createFilmsList } from './view/films';
+import { createFilmsList, createCardRate } from './view/films';
 
 const render = (container, place, template) => {
   container.insertAdjacentHTML(place, template);
@@ -11,7 +11,8 @@ const header = document.querySelector('.header');
 render(header, 'beforeend', createUserTemplate());
 
 const main = document.querySelector('.main');
-
 render(main, 'beforeend', createMenuTemplate());
 render(main, 'beforeend', createSortTemplate());
 render(main, 'beforeend', createFilmsList());
+const films = main.querySelector('.films');
+render(films, 'beforeend', createCardRate());
